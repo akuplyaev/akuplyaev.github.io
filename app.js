131c771f688a3fd6d057db05428fea3ba4f8a778
Alexey Kuplyaev <alexey.kuplyaev@altkraft.com>
2018-06-26 16:25:51 +0300

    dasdas

--- app.js
+++ app.js
@@ -45,11 +45,12 @@ messaging.onMessage(
 
 
 sendSubscriptionToServerForSave = function (token) {
-    console.log("wtf");
-    console.log(token);
     fetch("http://kuplyaev.local/api/v1.1/resources/push_subscribe?token=e36ffb6e4e3b4230a8ef200f7d91a4b6", {
         method: 'post',
         credentials: 'include',
+        headers: {
+            'content-type': 'application/json'
+        },
         body: JSON.stringify(Object.assign({}, {}, {
             "resource_token": "vujNq8yMTDg-8bd58a5e46439e8f",
             "subscriptions": [{
