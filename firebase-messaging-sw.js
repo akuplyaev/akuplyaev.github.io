68f70dde5972999abacb0061a9b9b8ba9194f3b7
Alexey Kuplyaev <alexey.kuplyaev@altkraft.com>
2018-06-28 12:21:34 +0300

    asdas

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,3 +23,27 @@ messaging.setBackgroundMessageHandler(
         return self.registration.showNotofication(title, options);
     }
 );
+
+
+
+// обработка клика на уведомлении
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(
+        clients.matchAll({
+            type: "window"
+        })
+        .then(function (clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+                var client = clientList[i];
+                if (client.url == '/' && 'focus' in client)
+                    return client.focus();
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(payload.data.action);
+            }
+        })
+    );
+});
