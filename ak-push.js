8a982b9285fec81735f57f8481be8e41b1450419
Alexey Kuplyaev <alexey.kuplyaev@altkraft.com>
2018-07-06 13:08:42 +0300

    asdasd

--- ak-push.js
+++ ak-push.js
@@ -1,3 +1,5 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 // akPushConfig = {
 //     ResourceToken: 'resource-token',
 //     ServerHost: 'server-host',
